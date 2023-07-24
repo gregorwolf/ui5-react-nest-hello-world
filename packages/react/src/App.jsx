902bde9990935171226f9ba7a15ae9028bf6a99f
Gregor Wolf <gregor.wolf@gmail.com>
2023-07-24 09:30:49 +0200

    adjust react app content

--- packages/react/src/App.jsx
+++ packages/react/src/App.jsx
@@ -7,7 +7,7 @@ function App() {
 
   return (
     <>
-      <h1>React</h1>
+      <h1>React Couter</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
